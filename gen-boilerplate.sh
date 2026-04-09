e82c19b7cfa6f0edb297ef20545fd67aebc7bd18
NodeMixaholic <no@nope.nicetry>
2026-04-08 20:12:03 -0500

    put the code in the repo.

---
+++ gen-boilerplate.sh
@@ -0,0 +1,27 @@
+read -p "Enter filename [feed.xml]: " fname
+fname="feed.xml"
+
+bptemplate=$(cat "template.xml.dontchange")
+wpfinal=$(cat "template.html.dontchange")
+
+read -p "Enter channel title: " rsschanneltitle
+read -p "Enter the main url for your site: " rssmainpage
+read -p "Enter channel description: " rsschanneldescription
+read -p "Enter channel language [en-us]: " rsschannellang
+rsschannellang=${rsschannellang:-"en-us"}
+
+bpfinal=$(echo "${bptemplate/rss-channel-lang/$rsschannellang}")
+
+bpfinal=$(echo "${bpfinal/rss-channel-title/$rsschanneltitle}")
+wpfinal=$(echo "${wpfinal/rss-channel-title/$rsschanneltitle}")
+
+bpfinal=$(echo "${bpfinal/rss-channel-description/$rsschanneldescription}")
+wpfinal=$(echo "${wpfinal/rss-channel-description/$rsschanneldescription}")
+
+bpfinal=$(echo "${bpfinal/rss-mainpage/$rssmainpage}")
+
+echo $bpfinal > $fname
+echo $wpfinal > index.html
+
+echo Done generating boilerplate, now making first post...
+./post.sh
---
+++ post.sh
@@ -0,0 +1,32 @@
+posttemplate=$(cat "post-template.xml.dontchange")
+webtemplate=$(cat "post-template.html.xml.dontchange")
+fname="feed.xml"
+pname="index.html"
+read -p "Enter post title: " rssposttitle
+read -p "Enter post author: " rsspostauthor
+read -p "Press ENTER to use nano to enter post contents..."
+randomfn="/tmp/post-content-$RANDOM"
+nano $randomfn
+rsspostcontent=$(cat $randomfn)
+
+echo A1
+rsspostdate=$(date -R)
+echo A2
+postfinal=$(echo "${posttemplate/post-title/$rssposttitle}")
+wpostfinal=$(echo "${webtemplate/post-title/$rssposttitle}")
+echo A3
+postfinal=$(echo "${postfinal/post-author/$rsspostauthor}")
+wpostfinal=$(echo "${wpostfinal/post-author/$rsspostauthor}")
+echo A4
+postfinal=$(echo "${postfinal/post-content/$rsspostcontent}")
+wpostfinal=$(echo "${wpostfinal/post-content/$rsspostcontent}")
+echo A5
+postfinal=$(echo "${postfinal/post-date/$rsspostdate}")
+wpostfinal=$(echo "${wpostfinal/post-date/$rsspostdate}")
+
+oldcontent=$(cat $fname)
+oldcontentwww=$(cat $fname)
+echo "${oldcontent/'</channel>'/$postfinal}" > $fname
+echo "${oldcontentwww/'</body>'/$wpostfinal}" > $pname
+
+echo Done.
